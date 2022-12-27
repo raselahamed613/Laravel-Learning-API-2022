cd2bc7e4665f565f7888401538bc8ec419289bc8
devshaik444 <mamin172025@bsces.uiu.ac.bd>
2022-12-27 17:54:27 +0600

    Last Commit: php database connection 27 december 2022

--- PHP/practice/database/db.php
+++ PHP/practice/database/db.php
@@ -9,6 +9,38 @@ $result = mysqli_query($conn, $sql);
 // print_r($data);
 // print_r($data1);
 
+
 mysqli_close($conn);
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User List</title>
+</head>
+<body>
+    <table border = "1">
+        <tr>
+            <td>ID</td>
+            <td>USERNAME</td>
+            <td>PASSWORD</td>
+            <td>EMAIL</td>
+            <td>TYPE</td>
+        </tr>
+<?php while($data = mysqli_fetch_assoc($result)){ ?>
+    <tr>
+        <td><?php echo $data['id']?></td>
+        <td><?php echo $data['username']?></td>
+        <td><?php echo $data['password']?></td>
+        <td><?php echo $data['email']?></td>
+        <td><?php echo $data['type']?></td>
+    </tr>
+<?php } ?>
+</table>
+     
+
+</body>
+</html>
