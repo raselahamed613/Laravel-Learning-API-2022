9679d5aa052ae77bb6d95564f64190192b24727f
devshaik444 <mamin172025@bsces.uiu.ac.bd>
2023-01-14 14:37:13 +0600

    Show Database Data on Website 14 january 2023

---
+++ PHP/2023/show-database-data-on-website/index.php
@@ -0,0 +1,39 @@
+<?php
+    include_once 'includes/dbh.php';
+?>
+
+<!DOCTYPE html>
+<html>  
+<head>
+    <title></title>
+</head>
+<body>
+    <!-- <h1>Hi</h1> -->
+    <?php
+        // $sql = "SELECT * FROM users;";
+        $sql = "SELECT * FROM users WHERE user_first = 'Rasel';";
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
+
+        if($resultCheck > 0){
+            while ($row = mysqli_fetch_assoc($result)){
+                // echo "<table>";
+                // echo "<tr>";
+                // echo "<td>USER NAME</td>";
+                // echo "<td>PASSWOARD</td>";
+                // echo "</tr>";
+                // echo "<tr>";
+                // echo "<td> rasel </td>";
+                // // echo "<td>$row['user_pwd']</td>";
+                // echo "<td>123</td>";
+                // echo "</tr>";
+                // echo "</table>";
+                // echo "<br>";
+                echo $row['user_uid'] . $row['user_pwd']."<br>";
+
+            }
+        }
+        
+    ?>
+</body>
+</html>
