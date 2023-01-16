0cd45ecfcf59b075f0e9f0f66a8dfb01f694f057
devshaik444 <mamin172025@bsces.uiu.ac.bd>
2023-01-16 16:10:12 +0600

    16 january 2023 office commit

--- PHP/2023/show-database-data-on-website/attendance.php
+++ PHP/2023/show-database-data-on-website/attendance.php
@@ -1,7 +1,8 @@
 <!-- practice data show from database -->
 <?php
 include_once 'includes/dbh.php';
-$sql = "select * from log";
+// $sql = "select * from log";
+$sql = "SELECT DISTINCT card_no FROM log";
 // $sql = "SELECT * FROM log_data WHERE card_number='0013159803' AND (time BETWEEN '2023-01-10 00:00:01' and '2023-01-10 23:59:59') AND id ORDER BY ID ASC LIMIT 1";
 // $sql = 'SELECT * FROM log_data WHERE card_number=0013159803';
 // $sql = "SELECT * FROM log WHERE card_no='0013159803' AND (time BETWEEN '2023-01-01 00:00:01' and '2023-01-14 23:59:59') AND id ORDER BY ID ASC";
@@ -32,7 +33,8 @@ $result = mysqli_query($conn, $sql);
                 <td></td>
                 <td><?php echo $data['card_no'] ?></td>
                 <?php
-                    $sql1 = "SELECT * FROM log WHERE card_no= '".$data['card_no']." '  AND (time BETWEEN '2023-01-01 00:00:01' and '2023-01-14 23:59:59') AND id ORDER BY ID ASC";
+                    // $sql1 = "SELECT * FROM log WHERE card_no= '".$data['card_no']." '  AND (time BETWEEN '2023-01-01 00:00:01' and '2023-01-14 23:59:59') AND id ORDER BY ID ASC";
+                    $sql1 = "SELECT * FROM log WHERE card_no= '".$data['card_no']."' AND (DATE(time) BETWEEN '2023-01-01' and '2023-01-31') GROUP BY DATE(time) ORDER BY ID ASC";
                     $result1 = mysqli_query($conn, $sql1);
                     while ($data1 = mysqli_fetch_assoc($result1)) {
                 ?>
